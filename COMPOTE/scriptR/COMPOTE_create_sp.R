5d9e3dc0c24901098ed49f4a4320950f60d59d31
Etienne Delay <etienne.delay@cirad.fr>
2018-06-12 18:03:37 +0200

    folder names

--- COMPOTE/scriptR/COMPOTE_create_sp.R
+++ COMPOTE/scriptR/COMPOTE_create_sp.R
@@ -3,9 +3,10 @@
 library(NLMR)
 
 #set.seed(123)
+setwd("~/github/MAPS-11/COMPOTE/")
 
 ## generate polygon
-for (i in 1:10){
+for (i in 1:100){
   # simulate polygonal landscapes
   mosaicgibbs <- nlm_mosaicgibbs(ncol = 100,
                                  nrow = 100,
@@ -15,13 +16,13 @@ for (i in 1:10){
   # visualize the NLM
   #rasterVis::levelplot(mosaicgibbs, margin = FALSE, par.settings = rasterVis::viridisTheme())
   write.csv(matrix(values(mosaicgibbs), nrow = 100, ncol = 100),
-            file = paste0("/tmp/poly_matrix",i,".csv"), row.names = F)
+            file = paste0("nlogo/data/polygon/polygon_",i,".csv"), row.names = F)
 }
 
 ## generate random
 for(i in 1:10){
   write.csv(matrix(sample(seq(from = 0, to = 1, by = 0.5), size = 10000, replace = TRUE),
             nrow = 100, ncol = 100),
-            file = paste0("/tmp/random_matrix",i,".csv"), row.names = F)
+            file = paste0("nlogo/data/random/random",i,".csv"), row.names = F)
 }
 
