b0a5474a0edc973d51c422a8c41086b30c27bade
pioucyril <cyril.piou@cirad.fr>
2018-06-12 16:27:00 +0200

    submodels

--- COMPOTE/scriptR/submodels.R
+++ COMPOTE/scriptR/submodels.R
@@ -1,4 +1,4 @@
-#fonction de croissance de la production
+#fonction de croissance de la production     #croissance logistique
 
 r<-0.25
 Infest<-0
@@ -10,22 +10,24 @@ prodt0<-prodt0+r*prodt0*(1-prodt0/(prodMax*(1-Infest)))
 prodt1<-c(prodt1,prodt0)
 }
 
-plot(1:30,prodt1)
+plot(1:30,prodt1,type="l")
 
-# fonction d'apparition d'infestation   #Logistic TODO
+# fonction Logistique de probabilité d'apparition d'infestation  et de  valeur d'infestation
 
-pot<-seq(0,1,length=1000)
-alpha<-0.0005
-ProbaInfest = 1-alpha^(pot)
+pot<-seq(0,10,length=1000)
+alpha<-10
+betap<-0.5
+ProbaInfest =  pot/(1+ exp(-( alpha*(pot-betap))))
+ProbaInfest[ProbaInfest>1]<-1
 
-plot(pot,ProbaInfest)
+plot(pot,ProbaInfest,type="l")
 
 
 # fonction d'integration de potentiels
-rmax<-5
+rmax<-2
 sigma<-rmax/2
 distance <- seq(0,rmax,length=100)
 Infest<-0.01
-pott<- Infest * (1/sqrt(2*pi*sigma^2))*exp(-(distance^2 / (2*sigma^2) ))
+pott<- Infest * 1/rmax * (1/sqrt(2*pi*sigma^2))*exp(-(distance^2 / (2*sigma^2) ))
 
-plot(distance,pott)
+plot(distance,pott,type="l")
