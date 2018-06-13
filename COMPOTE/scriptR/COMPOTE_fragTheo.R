e9da71ed035313f574ac39cd6debf7914d14ea5c
Etienne Delay <etienne.delay@cirad.fr>
2018-06-13 23:43:38 +0200

    calcul sparial frag for each landscape

---
+++ COMPOTE/scriptR/COMPOTE_fragTheo.R
@@ -0,0 +1,33 @@
+#
+# setwd(dir = "~/Téléchargements/")
+# build()
+# install.packages("rasterFrag_0.0-1.tar.gz", repos = NULL, type = "source")
+
+library(raster)
+library(rasterFrag)
+
+setwd(dir = "~/github/MAPS-11/COMPOTE/")
+
+csv.l <- list.files(path = "nlogo/data/")
+csv.l <- csv.l[csv.l != "farmers.csv"]
+
+Ftheo <- prepareFragmentationTheo(nrepl=100,nrow=100,ncol=100)
+Ftheo <- theo
+
+my.df <- NULL
+for(i in 1:length(csv.l)){
+  a <- raster(as.matrix(read.csv(paste0("nlogo/data/",csv.l[i]))))
+  print(paste0("nlogo/data/",csv.l[i]))
+  if_v1 <- indexFrag(a,threshold = 0,theo = Ftheo)[[1]]
+  if_v2 <- indexFrag(a,threshold = 0.5,theo = Ftheo)[[1]]
+  if_v3 <- indexFrag(a,threshold = 1,theo = Ftheo)[[1]]
+  v <- c(csv.l[i], if_v1, if_v2, if_v3)
+  my.df <- rbind(my.df, v)
+}
+
+my.df <- as.data.frame(my.df)
+names(my.df)<-c("file","if1","if2","if3")
+
+write.csv(my.df, file = "nlogo/result/fragmentation_space.csv", row.names = T)
+
+
